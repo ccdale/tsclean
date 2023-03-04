c6b20e2892501e75b9e7aac6938b9424379d51b1
ccdale <chris.allison@hivehome.com>
2023-03-04 02:40:20 +0000

    Bumping version from 0.1.3 to 0.1.4

---
+++ src/tsclean/cleants.py
@@ -0,0 +1,38 @@
+import argparse
+import os
+import sys
+
+from tsclean import __version__, errorExit, errorNotify, errorRaise
+from tsclean.ffmpeg import tsClean
+
+
+def parseInput():
+    """Parses any command line arguments"""
+    try:
+        parser = argparse.ArgumentParser(
+            description="Clean broadcast transport stream files recorded from Freeview (UK DVB)."
+        )
+        parser.add_argument(
+            "-v", "--version", action="version", version=f"%(prog)s {__version__}"
+        )
+        # parser.add_argument(
+        #     "-f", "--force", help="Invalidate cached information", action="store_true"
+        # )
+        parser.add_argument("filename", help="path and filename of file to clean.")
+        args = parser.parse_args()
+        filename = " ".join(args.filename)
+        filename = filename.strip()
+        filename = os.path.abspath(os.path.expanduser(filename))
+        if not os.path.exists(filename):
+            raise Exception(f"file {filename} does not exist")
+        return filename
+    except Exception as e:
+        errorExit(sys.exc_info()[2], e)
+
+
+def doClean():
+    try:
+        filename = parseInput()
+        ofn = tsClean(fqfn)
+    except Exception as e:
+        errorNotify(sys.exc_info()[2], e)
