68c768ab90bb62c3ba616b87edd628dad09f4209
ccdale <chris.allison@hivehome.com>
2023-03-05 11:09:03 +0000

    Bumping version from 0.2.13 to 0.2.14

--- src/tsclean/__init__.py
+++ src/tsclean/__init__.py
@@ -1,6 +1,6 @@
 import sys
 
-__version__ = "0.2.13"
+__version__ = "0.2.14"
 
 
 def errorNotify(exci, e, fname=None):
--- tests/test_tsclean.py
+++ tests/test_tsclean.py
@@ -15,7 +15,7 @@ class TheException(Exception):
 
 
 def test_tsclean_version():
-    assert __version__ == "0.2.13"
+    assert __version__ == "0.2.14"
 
 
 def test_errorNotify(capsys):
