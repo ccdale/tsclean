3951d186894062edb66dbcf7d1d2d605b0ef2b9f
ccdale <chris.allison@hivehome.com>
2023-03-05 14:04:08 +0000

    Bumping version from 0.2.16 to 0.3.0

--- src/tsclean/__init__.py
+++ src/tsclean/__init__.py
@@ -1,6 +1,6 @@
 import sys
 
-__version__ = "0.2.16"
+__version__ = "0.3.0"
 
 
 def errorNotify(exci, e, fname=None):
--- tests/test_tsclean.py
+++ tests/test_tsclean.py
@@ -15,7 +15,7 @@ class TheException(Exception):
 
 
 def test_tsclean_version():
-    assert __version__ == "0.2.16"
+    assert __version__ == "0.3.0"
 
 
 def test_errorNotify(capsys):
