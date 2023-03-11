93674fc6afba5b2b83c08fdb06b3a17fdd3e0c04
ccdale <chris.allison@hivehome.com>
2023-03-11 13:50:44 +0000

    Bumping version from 0.4.0 to 0.5.0

--- src/tsclean/__init__.py
+++ src/tsclean/__init__.py
@@ -1,6 +1,6 @@
 import sys
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 
 
 def errorNotify(exci, e, fname=None):
--- tests/test_tsclean.py
+++ tests/test_tsclean.py
@@ -15,7 +15,7 @@ class TheException(Exception):
 
 
 def test_tsclean_version():
-    assert __version__ == "0.4.0"
+    assert __version__ == "0.5.0"
 
 
 def test_errorNotify(capsys):
