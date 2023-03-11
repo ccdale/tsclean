71426745c722ada525d22b35b845610cfe60c651
ccdale <chris.allison@hivehome.com>
2023-03-11 14:26:33 +0000

    Bumping version from 0.5.0 to 0.5.1

--- src/tsclean/__init__.py
+++ src/tsclean/__init__.py
@@ -1,6 +1,6 @@
 import sys
 
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 tvhuser = "unset"
 tvhpass = "unset"
 tvhipaddr = "druidmedia"
--- tests/test_tsclean.py
+++ tests/test_tsclean.py
@@ -15,7 +15,7 @@ class TheException(Exception):
 
 
 def test_tsclean_version():
-    assert __version__ == "0.5.0"
+    assert __version__ == "0.5.1"
 
 
 def test_errorNotify(capsys):
