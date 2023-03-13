216c1dc649c4d4aef8b6da5c602dc2a311f5c418
ccdale <chris.allison@hivehome.com>
2023-03-13 07:08:11 +0000

    obtain a valid remote filename first for testing

--- tests/test_radio.py
+++ tests/test_radio.py
@@ -1,11 +1,20 @@
 import os
 
 import tsclean
+from tsclean.config import getConfig
 from tsclean.radio import copyRadioFile
+from tsclean.tvh import getRadioRecorded
 
 
 def test_copyRadioFile():
-    show = {"filename": "/var/lib/tvheadend/radio/The Wilsons Save The World-1.ts"}
+    cfg = getConfig()
+    tsclean.tvhipaddr = cfg.get("tvhipaddr", "")
+    tsclean.tvhuser = cfg.get("tvhuser", "")
+    tsclean.tvhpass = cfg.get("tvhpass", "")
+    tsclean.sshuser = cfg.get("sshuser", "")
+    tsclean.sshhost = cfg.get("sshhost", "")
+    rrecs = getRadioRecorded()
+    show = rrecs[0]
     tsclean.radiooutputdir = "/tmp"
     copyRadioFile(show)
     oppath = os.path.abspath(os.path.expanduser(tsclean.radiooutputdir))
@@ -13,3 +22,5 @@ def test_copyRadioFile():
     basefn = fn.split("/")[-1]
     dest = f"{oppath}/{basefn}"
     assert os.path.exists(dest)
+    os.unlink(dest)
+    assert not os.path.exists(dest)
