ec906991306c4f67d3bfdf9dd6d33a0ad4087d6d
ccdale <chris.allison@hivehome.com>
2023-03-13 07:32:45 +0000

    start of doRadio function

--- src/tsclean/radio.py
+++ src/tsclean/radio.py
@@ -5,7 +5,13 @@ from fabric import Connection
 
 import tsclean
 from tsclean import errorExit, errorNotify, errorRaise
-from tsclean.shell import listCmd, shellCommand
+from tsclean.ffmpeg import makeAudioFile
+
+# TODO
+# DONE - Copy file from druidmedia
+# convert file to mp3
+# tag file with show information
+# move file into the radio directory hierarchy
 
 
 def copyRadioFile(show):
@@ -13,11 +19,23 @@ def copyRadioFile(show):
         fn = show["filename"]
         basefn = fn.split("/")[-1]
         oppath = os.path.abspath(os.path.expanduser(tsclean.radiooutputdir))
-        # print(f"{oppath=}")
         os.makedirs(oppath, exist_ok=True)
         dest = f"{oppath}/{basefn}"
-        # print(f"{dest=}")
         with Connection(host=tsclean.sshhost, user=tsclean.sshuser) as c:
             c.get(show["filename"], dest)
+        return dest
+    except Exception as e:
+        errorNotify(sys.exc_info()[2], e)
+
+
+def doRadio(show):
+    try:
+        src = copyRadioFile(show)
+        fn = show["filename"]
+        basefn = fn.split("/")[-1].split(".")[0]
+        dest = "/".join([os.path.dirname(src), basefn, ".mp3"])
+        mp3 = makeAudioFile(src, dest)
+        if mp3 is None:
+            raise Exception(f"failed to create mp3 from {src}")
     except Exception as e:
         errorNotify(sys.exc_info()[2], e)
