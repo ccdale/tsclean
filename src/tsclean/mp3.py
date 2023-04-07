dc3478e3ce34da1cbab4d025e0253ca1cf070cda
ccdale <chris.allison@hivehome.com>
2023-04-07 11:50:09 +0100

    sort out the input arguments

--- src/tsclean/mp3.py
+++ src/tsclean/mp3.py
@@ -10,45 +10,41 @@ def parseInput():
     """Parses any command line arguments"""
     try:
         parser = argparse.ArgumentParser(
-            description="Clean broadcast transport stream files recorded from Freeview (UK DVB)."
+            description="Extract the audio from a youtube video, convert it to mp3, tag the mp3."
         )
+        parser.add_argument("-A", "--artist", help="Artist name")
+        parser.add_argument("-a", "--album", help="album name")
+        parser.add_argument("-g", "--genre", help="track genre")
+        parser.add_argument("-i", "--input", help="the input path and filename")
+        parser.add_argument("-n", "--tracknumber", help="tracknumber within the album")
+        parser.add_argument("-t", "--title", help="Title of the track")
         parser.add_argument(
             "-v",
             "--version",
             action="version",
             version=f"%(prog)s {tsclean.__version__}",
         )
-        parser.add_argument(
-            "-f",
-            "--force",
-            help="Overwrite input file with output file if sanity tests pass.",
-            action="store_true",
-        )
-        parser.add_argument("-t", "--title", help="Title of the track")
-        parser.add_argument("-a", "--album", help="album name")
-        parser.add_argument("-g", "--genre", help="track genre")
-        parser.add_argument("-A", "--artist", help="Artist name")
-        parser.add_argument("-n", "--tracknumber", help="tracknumber within the album")
-        parser.add_argument(
-            "filename", help="path and filename of file to extract audio from."
-        )
+        parser.add_argument("outputfilename", help="the output path and filename")
         args = parser.parse_args()
-        # filename = " ".join(args.filename)
-        filename = args.filename.strip()
-        filename = os.path.abspath(os.path.expanduser(filename))
-        if not os.path.exists(filename):
-            raise Exception(f"file {filename} does not exist")
-        return filename, args
+        # argparser expandsuser and returns an absolute filename for file objects
+        if args.input is None:
+            raise Exception("Input (-i) file missing, use --help for help.")
+        if not os.path.exists(args.input):
+            raise Exception(f"input file {args.input} does not exist")
+        return args
     except Exception as e:
         errorExit(sys.exc_info()[2], e)
 
 
 def ytmp3():
+    """Extracts the audio from a youtube video, converts it to mp3, tags the mp3."""
     try:
-        filename, args = parseInput()
+        # argparse (above) returns the following structure (which isn't iterable):
+        # args=Namespace(force=False, title=None, album=None,
+        #    genre=None, artist=None, tracknumber=None,
+        #    filename='/home/chris/somefilename')
+        # the members can be accessed by `args.title` etc
+        args = parseInput()
+        print(f"{args=}")
     except Exception as e:
         errorNotify(sys.exc_info()[2], e)
-
-
-if __name__ == "__main__":
-    ytmp3()
