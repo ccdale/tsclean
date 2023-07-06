325f0b19801f41c259c0a46e34eee2441742db64
ccdale <chris.allison@hivehome.com>
2023-07-06 11:15:17 +0100

    Bumping version from 0.6.6 to 0.7.0

--- src/tsclean/__init__.py
+++ src/tsclean/__init__.py
@@ -1,6 +1,6 @@
 import sys
 
-__version__ = "0.6.6"
+__version__ = "0.7.0"
 tvhuser = "unset"
 tvhpass = "unset"
 tvhipaddr = "druidmedia"
--- tests/test_tsclean.py
+++ tests/test_tsclean.py
@@ -15,7 +15,7 @@ class TheException(Exception):
 
 
 def test_tsclean_version():
-    assert __version__ == "0.6.6"
+    assert __version__ == "0.7.0"
 
 
 def test_errorNotify(capsys):
